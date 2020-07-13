e10ca29b4982bf9f0a0f1aa000e76fd0bdeba0df
2181523 <alma03231013@gmail.com>
2020-07-13 17:42:53 +0900

    2020/7/13 スタッフリストの完成とコード修正 商品リストの追加画面作成

--- staff/staff_add_check.php
+++ staff/staff_add_check.php
@@ -8,50 +8,50 @@
 
 <?php
 
-$staff_name=$_POST['name'];
-$staff_pass=$_POST['pass'];
-$staff_pass2=$_POST['pass2'];
+$staff_name = $_POST['name'];
+$staff_pass = $_POST['pass'];
+$staff_pass2 = $_POST['pass2'];
 
-$staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-$staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
-$staff_pass2=htmlspecialchars($staff_pass2,ENT_QUOTES,'UTF-8');
+$staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
+$staff_pass=htmlspecialchars($staff_pass, ENT_QUOTES, 'UTF-8');
+$staff_pass2=htmlspecialchars($staff_pass2, ENT_QUOTES, 'UTF-8');
 
 if($staff_name=='')
 {
-    print'スタッフ名が入力されていません。<br/>';
+    print'スタッフ名が入力されていません。<br />';
 }
 else
 {
     print'スタッフ名:';
     print$staff_name;
-    print'<br/>';
+    print'<br />';
 }
 
 if($staff_pass=='')
 {
-    print'パスワードが入力されていません。<br/>';
+    print'パスワードが入力されていません。<br />';
 }
 
 if($staff_pass!=$staff_pass2)
 {
-    print'パスワードが一致しません。<br/>';
+    print'パスワードが一致しません。<br />';
 }
 
 if($staff_name==''||$staff_pass==''||$staff_pass!=$staff_pass2)
 {
     print'<form>';
-    print'<input type="button" onclick="history.back()"value="戻る">';
+    print'<input type = "button" onclick = "history.back()" value = "戻る">';
     print'</form>';
 }
 else
 {
-    $staff_pass=md5($staff_pass);
-    print'<form method="post" action="staff_add_done.php">';
-    print'<input type="hidden" name="name" value="'.$staff_name.'">';
-    print'<input type="hidden" name="pass" value="'.$staff_pass.'">';
-    print'<br/>';
-    print'<input type="button" onclick="history.back()" value="戻る">';
-    print'<input type="submit" value="OK">';
+    $staff_pass = md5($staff_pass);
+    print'<form method = "post" action = "staff_add_done.php">';
+    print'<input type = "hidden" name = "name" value="'.$staff_name.'">';
+    print'<input type = "hidden" name = "pass" value="'.$staff_pass.'">';
+    print'<br />';
+    print'<input type = "button" onclick = "history.back()" value = "戻る">';
+    print'<input type = "submit" value = "OK">';
     print'</form>';
 }
 
--- staff/staff_list.php
+++ staff/staff_list.php
@@ -28,7 +28,7 @@ print'スタッフ一覧<br /><br />';
 print'<form method="post"action="staff_branch.php">';
 while(true)
 {
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
     if($rec==false)
     {
             break;
@@ -37,6 +37,8 @@ while(true)
     print $rec['name'];
     print'<br />';
 }
+print'<input type="submit" name="disp" value="参照">';
+print'<input type="submit" name="add" value="追加">';
 print'<input type="submit" name="edit" value="修正">';
 print'<input type="submit" name="delete" value="削除">';
 print'</form>';
